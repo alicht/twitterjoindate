86835141653f48464e9d412fdb71b33761767b1b
Avi Lichtschein <AviLichtschein@gmail.com>
2014-09-23 02:14:01 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  
+  def search
+      
+  end
+
+  def search_results
+    twitter_client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['twitter_consumer_key']
+      config.consumer_secret     = ENV['twitter_consumer_secret']
+      config.access_token        = ENV[:twitter_access_token]
+      config.access_token_secret = ENV[:twitter_access_token_secret]
+    end
+    @name = params[:name]
+  end
+
+end
