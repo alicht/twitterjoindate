cde4030b446b2267db7688df852e5d978f558b6d
Avi Lichtschein <AviLichtschein@gmail.com>
2014-11-26 23:11:45 -0500

    got NYTimes API to start to work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -23,15 +23,18 @@ class WelcomeController < ApplicationController
      @full_name = user.name
      @created_at = user.created_at    
      @user_id = user.id
-     response = HTTParty.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&sort=oldest&q=&begin_date=#{@created_at}&end_date=#{@created_at}&api-key=5dc6806f7b4ce918bdc37bc975fb783a:10:69926712")
-     @nytimes_headlines = response["docs"][0]["headline"]
-
-
+     @created = user.created_at.strftime("%Y%m%d")
+     response = HTTParty.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&sort=oldest&begin_date=#{@created}&end_date=#{@created}&api-key=5dc6806f7b4ce918bdc37bc975fb783a:10:69926712")
+     @nytimes_headlines = response["response"]["docs"].map do |docs_hash|
+      {headline:  docs_hash["headline"]["main"], url: docs_hash["web_url"]}
+      # docs_hash["headline"]["main"]
+      # docs_hash["web_url"]
+     end
     end
 
 
-
-    # http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key5dc6806f7b4ce918bdc37bc975fb783a:10:69926712
+# http://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&sort=oldest&q=&begin_date="#{@created_at}"&end_date="#{@created_at}"&api-key=5dc6806f7b4ce918bdc37bc975fb783a:10:69926712
+# http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key5dc6806f7b4ce918bdc37bc975fb783a:10:69926712
 
   end
 
